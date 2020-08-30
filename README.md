c151bb7e15fc5f5a350ec36e320b5a90af0b5872
atthana-p <atthana.p@codium.co>
2020-08-30 18:41:06 +0700

    Updated main.ts

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 ## Rules
 
-+ A game is won by the first player to have won at least four points in total and at least two points more than the opponent.
++ A game is won by the first player to have won at least four points in total and at least two points more than the opponent. 4-x, p1-p2 > 2
 + The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as “love”, “fifteen”, “thirty”, and “forty” respectively.
-+ If at least three points have been scored by each player, and the scores are equal, the score is “deuce”.
-+ If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is “advantage” for the player in the lead.
++ If at least three points have been scored by each player, and the scores are equal, the score is “deuce”. 3-3
++ If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is “advantage” for the player in the lead. 4-3
 
 ## Alternate explanations
 
 + Each player can have either of these points in one game 0 15 30 40
 + If you have 40 and you win the ball you win the game, however there are special rules.
 + If both have 40 the players are deuce. a. If the game is in deuce, the winner of a ball will have advantage and game ball. b. If the player with advantage wins the ball he wins the game c. If the player without advantage wins they are back at deuce.
+
+
+---
+#Example
+round   p1  p2
+1       -   -  
+2       -   -
+3
+...
+6       3   3   deuce
+7       4   3   p1 is advantage   
+8       4   4   is deuce
+
+If win: 4-2
+If 4-3: p1 is advantage.
+If 3-3: deuce need to continue play to advantage in a 2 points.
--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
 //
 // Your implementations
-// 
+//
+
+function updateGameScore(
+    player1Points: number,
+    player2Points: number
+  ): { player1Points: number; player2Points: number } {
+    printScore([player1Points, player2Points]);
+    return {
+      player1Points,
+      player2Points,
+    };
+  }
+  
+  
+  
+  function decideGameWinner(gameResult: Game) {
+    if (gameResult.player1Point >= 4 && gameResult.player2Point >= 4) {
+      if (Math.abs(gameResult.player1Point[0] - gameResult.player1Point[1]) >= 2) {
+        return gameResult.player1Point[0] - gameResult.player1Point[1] > 0
+          ? 'Player 1'
+          : 'Player 2';
+      }
+    }
+  
+  }
+  
+  function printScore(playerScoreArray: number[]) {
+    if (!Array.isArray(playerScoreArray)) {
+      return;
+    }
+  
+    if (playerScoreArray.length !== 2) {
+      return;
+    }
+  
+    if (playerScoreArray.some((score) => score > 3)) {
+      if (playerScoreArray[0] === playerScoreArray[1]) {
+        return 'deuce';
+      } else if (Math.abs(playerScoreArray[0] - playerScoreArray[1]) === 1) {
+        return playerScoreArray[0] - playerScoreArray[1] > 0
+          ? 'advantage player 1'
+          : 'advantage player 2';
+      } else {
+        
+      }
+    } else {
+      const scoreWord = playerScoreArray.map((eachPlayerScore) => {
+        switch (eachPlayerScore) {
+          case 0:
+            return 'love';
+          case 1:
+            return 'fifteen';
+          case 2:
+            return 'thirty';
+          case 3:
+            return 'forty';
+          default:
+            return;
+        }
+      });
+      return `${scoreWord[0]} - ${scoreWord[1]}`;
+    }
+  }
+  
+  interface Game {
+    player1Point: number;
+    player2Point: number;
+  }
